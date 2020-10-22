fe698f12b6448e445b3749ec244459ff19779057
CerealKillerMR <m.rahuoja@gmail.com>
2020-10-22 21:08:58 +0300

    sum more thresholding and beginning of serial com.

--- robot/robot1.py
+++ robot/robot1.py
@@ -2,21 +2,24 @@
 # 1. Show webcam
 # 2. Get colors from colors.json
 # 3. Display filtered image
-<<<<<<< HEAD
+import serial
 from collections import deque
-=======
-
-<<<<<<< HEAD
 from collections import deque
->>>>>>> 63e92b6d6233f5be3a133f3998f6a681b3b3e10d
-=======
->>>>>>> 6b67894856381e027699c99381cac1da1688c9fa
 from imutils.video import VideoStream
 import cv2
 import json
 import numpy as np
 import imutils
 
+#saada koordid "sd:0:0:0" kujul
+#gs = get speed
+
+#ser = serial.Serial('/dev/ttyACM0')  # open serial port
+#print(ser.name)         # check which port was really used
+ser=serial.Serial('/dev/ttyACM0', 115200, timeout=0.00001)
+ser.write(b'hello')     # write a string
+print(ser)
+ser.close()
 # Load saved color values from colors.json
 try:
     with open("colors.json", "r") as f:
@@ -37,10 +40,6 @@ while cap.isOpened():
     _, bgr = cap.read()
     #cv2.imshow("bgr", bgr)
     
-    #siin mingi jama, järgmine kord findcontours()
-    #bgr = imutils.resize(bgr, height=600)
-    #blurred = cv2.GaussianBlur(bgr, (11, 11), 0)
-    
     # 2. Convert BGR to HSV where color distributions are better
     hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
     #cv2.imshow("hsv", hsv)
@@ -55,12 +54,12 @@ while cap.isOpened():
     filtered_image = cv2.bitwise_or(bgr, bgr, mask=mask)
     blurred = cv2.GaussianBlur(filtered_image, (11, 11), 0)
     img_gray = cv2.cvtColor(blurred, cv2.COLOR_BGR2GRAY)
-    ret, im = cv2.threshold(img_gray, 65, 255, cv2.THRESH_BINARY_INV)
-    img, contours, hierarchy  = cv2.findContours(im, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #ret, im = cv2.threshold(filtered_image, 65, 255, cv2.THRESH_BINARY_INV)
+    img, contours, hierarchy  = cv2.findContours(img_gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     print("Number of Contours found = " + str(len(contours)))
-    #cv2.drawContours(img, contours, -1, (0,255,255), 5)
-    cv2.imshow("img", img)
-
+    cv2.drawContours(img, contours, -1, (0,255,255), 5)
+    
+    #cv2.imshow("filtered image", filtered_image)
     # 4. TODO: locate ball and basket coordinates
     # only proceed if at least one contour was found
     if len(contours) > 0:
@@ -69,18 +68,19 @@ while cap.isOpened():
 	    # centroid
 	    c = max(contours, key=cv2.contourArea)
 	    ((x, y), radius) = cv2.minEnclosingCircle(c)
-	    print("c:", c)
+	    #print("c:", c)
 	    M = cv2.moments(c)
 	    center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 	    print("This is the center:", center)
 	    # only proceed if the radius meets a minimum size
-	    """if radius > 10:
+	    if radius > 5:
 	    	# draw the circle and centroid on the frame,
 	    	# then update the list of tracked points
-	    	cv2.circle(img, (int(x), int(y)), int(radius),
+	    	cv2.circle(filtered_image, (int(x), int(y)), int(radius),
 	    		(0, 255, 255), 2)
-	    	cv2.circle(img, center, 5, (0, 0, 255), -1)"""
-                
+	    	cv2.circle(filtered_image, center, 5, (0, 0, 255), -1)
+    cv2.imshow("img", img)
+    cv2.imshow("filtered", filtered_image)
     
     key = cv2.waitKey(10)
 
@@ -88,3 +88,4 @@ while cap.isOpened():
         break
 
 cap.release()
+ser.close()
